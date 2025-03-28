541a42f9b67e871ad4aeac83d15e02fca8889f5c
cbecerratadei <cbecerratadei@mail.austral.edu.ar>
2025-03-28 11:51:45 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -2,7 +2,7 @@ def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
     espacios = " "
-        print(first_name.lower() + espacios + last_name.lower())
-        print(first_name.title() + espacios + last_name.title())
-        print(first_name.upper() + espacios + last_name.upper())
-        print(espacios * 8 + first_name.lower() + espacios + last_name.lower())
+    print(first_name.lower() + espacios + last_name.lower())
+    print(first_name.title() + espacios + last_name.title())
+    print(first_name.upper() + espacios + last_name.upper())
+    print(espacios * 8 + first_name.lower() + espacios + last_name.lower())
