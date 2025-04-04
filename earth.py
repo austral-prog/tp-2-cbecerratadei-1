cc61833682bf3bce26e05a78b5ae0544fdbd9fe8
cbecerratadei <cbecerratadei@mail.austral.edu.ar>
2025-04-04 12:27:56 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -5,4 +5,4 @@ def earth():
     comparación2 = y < x
     print(f"The result of {x} comes first in the dictionary than {y} is {comparación1}")
     print(f"The result of {y} comes first in the dictionary than {x} is {comparación2}")
-    print(earth())
+earth()
